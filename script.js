4eef1a16e6c1de20e82392c6355ab0bfe232c789
Hyungmon <rangga.godzila@gmail.com>
2021-11-22 21:17:10 +0700

    Re-arrange script.js

--- script.js
+++ script.js
@@ -25,35 +25,35 @@ gsap.from('.display-4', {
 })
 
 
-// Contact Form JS
-const scriptURL =
-    'https://script.google.com/macros/s/AKfycbw12CA-DsCvuyVlLPlNZizHfax065EUV_cJKN40sCllLCcVQKXxI2q8rbat5yslIskiKg/exec'
-const form = document.forms['github-contact-form']
-const btnKirim = document.querySelector('.btn-kirim')
-const btnLoading = document.querySelector('.btn-loading')
-const formAlert = document.querySelector('.form-alert')
+// // Contact Form JS
+// const scriptURL =
+//     'https://script.google.com/macros/s/AKfycbw12CA-DsCvuyVlLPlNZizHfax065EUV_cJKN40sCllLCcVQKXxI2q8rbat5yslIskiKg/exec'
+// const form = document.forms['github-contact-form']
+// const btnKirim = document.querySelector('.btn-kirim')
+// const btnLoading = document.querySelector('.btn-loading')
+// const formAlert = document.querySelector('.form-alert')
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    // ketika tombol submit diklik
-    // tampilkan tombol loading, hilangkan tombol kirim
-    btnLoading.classList.toggle('d-none')
-    btnKirim.classList.toggle('d-none')
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     // ketika tombol submit diklik
+//     // tampilkan tombol loading, hilangkan tombol kirim
+//     btnLoading.classList.toggle('d-none')
+//     btnKirim.classList.toggle('d-none')
 
-    fetch(scriptURL, {
-        method: 'POST',
-        body: new FormData(form)
-    })
-        .then(response => {
-            // tampilkan tombol kirim, hilangkan tombol loading
-            btnLoading.classList.toggle('d-none')
-            btnKirim.classList.toggle('d-none')
-            // tampilkan alert
-            formAlert.classList.toggle('d-none')
-            // reset form
-            form.reset()
+//     fetch(scriptURL, {
+//         method: 'POST',
+//         body: new FormData(form)
+//     })
+//         .then(response => {
+//             // tampilkan tombol kirim, hilangkan tombol loading
+//             btnLoading.classList.toggle('d-none')
+//             btnKirim.classList.toggle('d-none')
+//             // tampilkan alert
+//             formAlert.classList.toggle('d-none')
+//             // reset form
+//             form.reset()
 
-            console.log('Success!', response)
-        })
-        .catch(error => console.error('Error!', error.message))
-})
+//             console.log('Success!', response)
+//         })
+//         .catch(error => console.error('Error!', error.message))
+// })
